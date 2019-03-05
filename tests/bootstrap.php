01140bcafd73b1f931e0ca5828f989a70a6c22b2
João Rebelo <joaomfrebelo@gmail.com>
2019-03-05 22:46:31 +0000

    fix: Add the composer vendor autoload in the test bootstrap file

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,10 @@
 <?php
 
+require_once __DIR__
+    . DIRECTORY_SEPARATOR . ".."
+    . DIRECTORY_SEPARATOR . "vendor"
+    . DIRECTORY_SEPARATOR . "autoload.php";
+
 require_once ".."
     . DIRECTORY_SEPARATOR
     . "src"
